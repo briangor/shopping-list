c08afe6b0f7e34a8fd37755da5aed82b7d55d2e7
Brian <briangor13@gmail.com>
2021-03-23 17:26:58 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,17 +1,19 @@
 var app = angular.module("ShoppingListApp", []); 
 app.controller("shopCtrl", function($scope) {
     $scope.products = ["Milk", "Bread", "Cheese"];
-    $scope.addItem = function () {
+    
+    $scope.addItemFx = function () {
         $scope.errortext = "";
-        if (!$scope.addMe) {return;}        
-        if ($scope.products.indexOf($scope.addMe) == -1) {
-            $scope.products.push($scope.addMe);
+        if (!$scope.addItem) {return;}        
+        if ($scope.products.indexOf($scope.addItem) == -1) {
+            $scope.products.push($scope.addItem);
         } else {
             $scope.errortext = "The item is already in your shopping list.";
         }
-    }
+    };
+    
     $scope.removeItem = function (x) {
         $scope.errortext = "";    
         $scope.products.splice(x, 1);
-    }
+    };
 });
