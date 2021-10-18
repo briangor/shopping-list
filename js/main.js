b143770b84209176bee1307e66c45b7c5d25eae6
briangor <briangor13@gmail.com>
2021-10-18 11:05:19 -0400

    Added quantity to products

--- js/main.js
+++ js/main.js
@@ -11,18 +11,40 @@ app.controller("shopCtrl", function($scope) {
         $scope.maximize = true;
     }
     
-    $scope.products = ["Milk", "Bread", "Cheese", "Cake"];
+    //Variables
+    $scope.setQuantity;
+
+
+    //JSON file to store items
+    //$scope.products = ["Milk", "Bread", "Cheese", "Cake"];
+    $scope.products = [{
+        "title": "Milk",
+        "quantity": 2
+    },{
+        "title": "bread",
+        "quantity": 3
+    }];
     
+
     $scope.addItemFx = function () {
         $scope.errortext = "";
         if (!$scope.addItem) {return;}        
         if ($scope.products.indexOf($scope.addItem) == -1) {
-            $scope.products.push($scope.addItem);
+            //$scope.products.push($scope.addItem, $scope.setQuantity);
+            $scope.products.push({
+                title: $scope.addItem,
+                quantity: $scope.setQuantity 
+            });
+
+            console.log("$scope.addItem: " + $scope.addItem );
+            console.log("$scope.setQuantity "+ $scope.setQuantity);
         } else {
             $scope.errortext = "The item is already in your shopping list.";
         }
         $scope.addItem=" ";
+        $scope.setQuantity=0;
     };
+
     
     $scope.removeItem = function (x) {
         $scope.errortext = "";    
